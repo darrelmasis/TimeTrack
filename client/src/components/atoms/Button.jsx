154fe4ddba15f3fc57512f514184d9eb69d28e85
Darrel Masis <darrelmasis@gmail.com>
2023-10-15 09:24:24 -0600

    Refactoriza el componente Button
    Ahora el componente Button admite clases a través de la prop
    className

--- client/src/components/atoms/Button.jsx
+++ client/src/components/atoms/Button.jsx
@@ -1,6 +1,6 @@
-export const Button = ({ size, variant, isSquare, children, onClickCallBack }) => {
+export const Button = ({ size, variant, isSquare, children, onClickCallBack, className}) => {
   
-  const buttonClasses = `btn ${variant ? `btn-${variant}` : ''} ${size ? `btn-${size}` : ''} ${isSquare ? 'btn-square' : ''}`;
+  const buttonClasses = `btn ${variant ? `btn-${variant}` : ''} ${size ? `btn-${size}` : ''} ${isSquare ? 'btn-square' : ''}`.trim() + ` ${className ? className : ''}`;
 
   return (
     <button type="button" className={buttonClasses.trim()} onClick={onClickCallBack}>{ children }</button>
