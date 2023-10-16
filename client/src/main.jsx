a492e97ae9c80b632e359a29483bd89a09146041
Darrel Masis <darrelmasis@gmail.com>
2023-10-16 11:42:58 -0600

    Elimina la importación de Bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap' // Importa los archivos JS de Bootstrap
 import './styles/styles.scss' // Importa los estilos SCSS consolidados
 import { ThemeProvider } from './components/commons/ThemeContext.jsx'
 
