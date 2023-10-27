7e52eb05f1d96cd42bd0d02cf876287350e0ead3
Darrel Masis <darrelmasis@gmail.com>
2023-10-27 10:42:12 -0600

    Agrega el componente ImageSwitcher

--- client/src/pages/views/SignIn.jsx
+++ client/src/pages/views/SignIn.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react"  
 import { useUpdatePageTitle } from "../../App"
 import { Icon } from "../../components/commons/CustomIcons"
+import { ImageSwitcher } from "../../components/atoms/ImageSwitcher"
 import logo from "../../assets/images/logo-harinas-monisa.svg"
+import logoAlt from "../../assets/images/logo-harinas-monisa-alt.svg"
+
+const logoSources = {
+  light: logo,
+  dark: logoAlt
+}
 
 const Signin = () => {
 
@@ -9,13 +16,13 @@ const Signin = () => {
   const [showPassword, setShowPassword] = useState(false)
   return (
     <main className="container">
-      <section className="card bg-container border-0 shadow-lg mt-5 signin-screen rounded-4">
-        <div className="card-header bg-transparent border-bottom-0 text-center">
-          <img className="mb-3" width="100px" src={logo} alt="logo_monisa_harinas" />
-          <h1 className="h4 text-dark">Bienvenido</h1>
+      <section className="bg-container mt-5 rounded border p-3">
+        <div className="text-center">
+          <ImageSwitcher src={logoSources}>Logotipo de Harinas Monisa</ImageSwitcher>
+          <h1 className="">Bienvenido</h1>
         </div>
         <div className="card-body">
-          <form action="" id="signin-form">
+          <form action="/" method="POST">
             <div className="mb-3">
               <label className="form-label" htmlFor="signin-email">Correo electrónico</label>
               <input className="form-control" type="email" id="signin-email" placeholder="asistauditor@monisa.com" autoComplete="off"/>
@@ -26,7 +33,7 @@ const Signin = () => {
                 <input className="form-control" type={showPassword ? "text" : "password"} id="signin-password" placeholder="••••••••••" autoComplete="off" />
                 <div className="password-state-icon" onClick={() => setShowPassword(!showPassword)} >
                   {
-                    showPassword ? <Icon icon="eye-slash" classes="me-2" fixedWidth/> : <Icon icon="eye" classes="me-2" fixedWidth/>
+                    showPassword ? <Icon icon="eye-slash" classes="me-2" fix="size"/> : <Icon icon="eye" classes="me-2" fix="size"/>
                   }
                 </div>
               </div>
