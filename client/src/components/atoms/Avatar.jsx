95f7140a12fda7925639594ab665d862a1fab7ac
Darel Masis <darrelmasis@gmail.com>
2024-02-05 17:38:51 -0600

    Mejora considerablemente el component Avatar
    Tambien agrega el paquete initials para obtener las inicials del nombre del usuaio activo.

--- client/src/components/atoms/Avatar.jsx
+++ client/src/components/atoms/Avatar.jsx
@@ -3,6 +3,41 @@
  * si no se agrega un fuente de la imagen se mostrata lo que contenga el comopoente como hijo
  * el texto mostrado se tomará del nombre del usuario ej: John Doe = JD
  */
-export const Avatar = ({ src, alt, size, className, children }) => {
-  return <div className={`avatar avatar-${size} ${className}`}>{src ? <img src={src} alt={alt} /> : children}</div>
+
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import initials from 'initials'
+
+export const Avatar = ({ size, name, src, color, classes }) => {
+  const componentClasses = classNames('avatar', size && `avatar-${size}`, classes && classes)
+  const abbr = initials(name)
+  const [imageLoaded, setImageLoaded] = useState(true)
+
+  const handleImageLoad = () => {
+    // Se ejecuta cuando la imagen se carga correctamente
+    setImageLoaded(true)
+  }
+
+  const handleImageError = () => {
+    // Se ejecuta si hay un error al cargar la imagen
+    setImageLoaded(false)
+  }
+
+  let inner = ''
+
+  if (src) {
+    // Si hay una URL de imagen (src), muestra la imagen
+    inner = <img src={src} alt={name} onLoad={handleImageLoad} onError={handleImageError} />
+  }
+
+  // Si no hay una URL de imagen o la imagen no se ha cargado aún, muestra las iniciales
+  if (!src || !imageLoaded) {
+    inner = abbr
+  }
+
+  return (
+    <div className={componentClasses} style={{ backgroundColor: '#a6d6fa' }}>
+      {inner}
+    </div>
+  )
 }
--- client/src/components/organisms/Header.jsx
+++ client/src/components/organisms/Header.jsx
@@ -1,64 +1,65 @@
 import { Link } from 'react-router-dom'
-import { Dropdown } from '../molecules/Dropdown'
+import { Dropdown, DropdownMenu, DropdownItem, DropdownItemText, DropdownTrigger, DropdownDivider } from '../molecules/Dropdown'
 import { Avatar } from '../atoms/Avatar'
 import { Icon } from '../commons/CustomIcons'
 import { SwitchControl } from '../atoms/Switch'
-import { useState } from 'react'
 import { Button } from '../atoms/Button'
 
 export const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen)
+  const avatar = {
+    name: 'Darel Masis',
+    src: 'src/assets/images/d46112842.png',
   }
-
-  const headerButton = (
-    <Button onClickCallBack={toggleDropdown} className={`caret dropdown-button d-flex justify-content-space-between header-button`}>
-      <Avatar size="sm" className="me-2">
-        DM
-      </Avatar>
-      <span className="header-button-text">Darel</span>
-    </Button>
-  )
-
   return (
     <header className="main-header bg-container border-bottom">
       <nav className="navbar container py-3">
         <Link to="/" className="navbar-brand">
           TimeTrack
         </Link>
-
-        <Dropdown toggleButton={headerButton} isOpen={isOpen} setIsOpen={setIsOpen} position="bottom">
-          <div className="dropdown-item-text mb-3">
-            <Avatar size="md" className="me-2">
-              DM
-            </Avatar>
-            <div>
-              <div>Darel Masis</div>
-              <div title="Analista de Control Interno" className="job-title text-muted small">
-                Analista de Control Interno
+        <Dropdown>
+          <DropdownTrigger>
+            <Button classes={`caret d-flex justify-content-space-between header-button dropdown-button`}>
+              <Avatar size="sm" name={avatar.name} src={avatar.src} classes="me-2">
+                DM
+              </Avatar>
+              <span className="header-button-text">Darel</span>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItemText>
+              <Avatar size="md" name={avatar.name} src={avatar.src} classes="me-2">
+                DM
+              </Avatar>
+              <div>
+                <div>Darel Masis</div>
+                <div title="Analista de Control Interno" className="job-title text-muted small">
+                  Analista de Control Interno
+                </div>
               </div>
-            </div>
-          </div>
-          <a href="#" className="dropdown-item mb-2">
-            <Icon icon="circle-user" classes="me-2" />
-            Perfil
-          </a>
-          <a href="#" className="dropdown-item mb-2">
-            <Icon icon="gears" classes="me-2" />
-            Ajustes
-          </a>
-          <div className="dropdown-item mb-2">
-            <SwitchControl variant="success" switchId="themeChanger">
-              <Icon icon="theme" classes="me-2" />
-              <span>Tema Oscuro</span>
-            </SwitchControl>
-          </div>
-          <hr className="dropdown-divider"></hr>
-          <Link to="/" className="dropdown-item text-danger">
-            <Icon icon="logout" classes="me-2" /> Cerrar Sesión
-          </Link>
+            </DropdownItemText>
+            <DropdownItem>
+              <Icon icon="circle-user" classes="me-2" />
+              <span>Perfil</span>
+            </DropdownItem>
+
+            <DropdownItem>
+              <Icon icon="gears" classes="me-2" />
+              <span>Ajustes</span>
+            </DropdownItem>
+
+            <DropdownItem>
+              <SwitchControl variant="success" switchId="themeChanger">
+                <Icon icon="theme" classes="me-2" />
+                <span>Tema Oscuro</span>
+              </SwitchControl>
+            </DropdownItem>
+            <DropdownDivider />
+            <DropdownItem>
+              <Link to="/" className="text-danger">
+                <Icon icon="logout" classes="me-2" /> Cerrar Sesión
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
         </Dropdown>
       </nav>
     </header>
