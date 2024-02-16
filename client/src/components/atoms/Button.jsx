c42db1a73f01801b5574084c531493d1daf3f6dc
Darel Masis <darrelmasis@gmail.com>
2024-02-16 17:00:02 -0600

    refactor: refactoriza el componente para mejorar su estructura y funcionalidad.
    - Añande la capacidad de mostrar un icono en el botón.
    - Utiliza la prop label o children para mostrar contenido en el botón.

--- client/src/components/atoms/Button.jsx
+++ client/src/components/atoms/Button.jsx
@@ -1,19 +1,40 @@
+import React from 'react'
 import classNames from 'classnames'
+import { Icon } from '../commons/CustomIcons'
 
-export const Button = ({ size, variant, isSquare, children, onClick, classes, disabled }) => {
-  const buttonClasses = classNames('btn', size && `btn-${size}`, variant && `btn-${variant}`, isSquare && 'btn-square', classes)
+export const Button = ({ size = 'medium', variant, onClick, disabled, icon, label, children, classes }) => {
+  
+  const sizeToIconClass = {
+    small: 'me-1',
+    medium: 'me-2',
+    large: 'me-2',
+  };
 
+  const childrenExist = React.Children.count(children) > 0
+  const labelExist = label ? true : false
+  const iconClasses = classNames((childrenExist || labelExist) && sizeToIconClass[size])
+
+  const buttonType = (!labelExist && !childrenExist) ? 'btn-square' : false
+  const buttonClasses = classNames('btn', buttonType && buttonType, size && `btn-${size}`, variant && `btn-${variant}`, classes)
   const isDisabled = disabled ? true : false
 
+  const iconComponent = icon ? <Icon icon={icon} classes={iconClasses} /> : null;
   const handleOnclick = () => {
     if (onClick && !isDisabled) {
       onClick()
     }
   }
+  const buttonContent = (
+    <>
+      {iconComponent}
+      {childrenExist ? children : label}
+    </>
+  )
+
 
   return (
     <button type="button" disabled={isDisabled} className={buttonClasses} onClick={handleOnclick}>
-      {children}
+      {buttonContent}
     </button>
   )
 }
