487499b3d70dde848d5ccdf134a69748801e085f
Darel Masis <darrelmasis@gmail.com>
2024-02-17 07:29:44 -0600

    refactor: mejora la legibilidad del código del componente Button.
    - Elmina puntos y coma innesesarios.
    - Mejora la condicional para definir si l botón es square. Encerrando enparentesis la misma.

--- client/src/components/atoms/Button.jsx
+++ client/src/components/atoms/Button.jsx
@@ -3,27 +3,27 @@ import classNames from 'classnames'
 import { Icon } from '../commons/CustomIcons'
 
 export const Button = ({ size = 'medium', variant, onClick, disabled, icon, label, children, classes }) => {
-  
   const sizeToIconClass = {
     small: 'me-1',
     medium: 'me-2',
     large: 'me-2',
-  };
+  }
 
   const childrenExist = React.Children.count(children) > 0
   const labelExist = label ? true : false
   const iconClasses = classNames((childrenExist || labelExist) && sizeToIconClass[size])
 
-  const buttonType = (!labelExist && !childrenExist) ? 'btn-square' : false
+  const buttonType = !labelExist && !childrenExist ? 'btn-square' : false
   const buttonClasses = classNames('btn', buttonType && buttonType, size && `btn-${size}`, variant && `btn-${variant}`, classes)
   const isDisabled = disabled ? true : false
 
-  const iconComponent = icon ? <Icon icon={icon} classes={iconClasses} /> : null;
+  const iconComponent = icon ? <Icon icon={icon} classes={iconClasses} /> : null
   const handleOnclick = () => {
     if (onClick && !isDisabled) {
       onClick()
     }
   }
+  
   const buttonContent = (
     <>
       {iconComponent}
@@ -31,11 +31,9 @@ export const Button = ({ size = 'medium', variant, onClick, disabled, icon, labe
     </>
   )
 
-
   return (
     <button type="button" disabled={isDisabled} className={buttonClasses} onClick={handleOnclick}>
       {buttonContent}
     </button>
   )
 }
-
